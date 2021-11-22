0f28c6fff4d54e7b966e3cd292554fdf6a255fe0
jungsoo_cho <tmaxchita@gmail.com>
2021-11-22 15:09:44 +0900

    20211122 at work : add Paintbrush

--- App.java
+++ App.java
@@ -29,15 +29,38 @@ class MyCanvas extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Paintbrush myBrush = new Paintbrush(g);
+        myBrush.drawSky();
+        myBrush.drawMountains();
+
+    }
+
+}
+
+class Paintbrush {
+
+    private Graphics g;
+
+    public Paintbrush(Graphics graphics) {
+        g = graphics;
+    }
+
+    public void drawSky() {
         g.setColor(new Color(157, 233, 245));
         g.fillRect(0, 0, 900, 220);
 
         g.setColor(Color.yellow);
         g.fillOval(680, 30, 60, 60);
+    }
 
-
+    public void drawMountains() {
         g.setColor(Color.green);
-        g.fillOval(280, 30, 60, 60);
+        Polygon triangle = new Polygon();
+        triangle.addPoint(15, 450);
+        triangle.addPoint(390, 60);
+        triangle.addPoint(550, 450);
+
+        g.fillPolygon(triangle);
 
     }
 
